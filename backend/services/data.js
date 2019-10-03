249916fb2c4a0a805c4b19d66952a5c7e4798c0c
Jesse Vogel <Jvogel@amplify.com>
2019-10-03 17:51:06 -0400

    added data for 2018

--- backend/services/data.js
+++ backend/services/data.js
@@ -2,62 +2,101 @@ import path from 'path'
 import axios from 'axios'
 import fs from 'fs'
 
-/**
- * for every year
- *      get impact performance
- *      for every school
- *          get enrollment data
- *          get student population
- *          get framework scores
- *          percentile rank
- */
-
 const baseUrl = 'https://tools.nycenet.edu/dashboard/api/data/dashboard'
 
-const endpoints = {
+const startYear= 2018
+const endYear = 2018
 
+const scrapeEndpointData = async (schoolId, year, endpoint, formData, outputFile) => {
+    
+    if(fs.existsSync(outputFile)){
+        console.log(`data for ${schoolId} in ${year} already exists`)
+    } else {
+        const response = await axios(
+            {
+                method: 'post',
+                url: endpoint,
+                data: formData,
+                config: { headers: {'Content-Type': 'multipart/form-data' }}
+            }
+        )
+        
+        if(response.data.length > 0){
+            fs.writeFileSync(outputFile, JSON.stringify({data: response.data}))
+        } else {
+            console.log(`No data for: ${schoolId} in ${year} from ${endpoint}`)
+        }
+    }
 }
 
-const startYear= 2002
-const endYear = 2019
+const scrapeDataForYear = async (schoolsArray, year, outputDir) => {
+    console.log(`getting data for year: ${year}`)
+    const numSchools = schoolsArray.length
+    for(let schoolIndex = 0; schoolIndex < numSchools; schoolIndex++){
+        const schoolObj = schoolsArray[schoolIndex]
+        const schoolId = schoolObj.dbn
+        try {
+            const schoolIdDir = path.join(outputDir, `/${schoolId}`)
+            if (!fs.existsSync(schoolIdDir)){
+                fs.mkdirSync(schoolIdDir);
+            }
+
+            const formData = `dbn=${encodeURIComponent(schoolId)}&report_type=${encodeURIComponent(schoolObj["report_type"])}&report_year=${encodeURIComponent(year)}`
+
+            const enrollmentOutputFile = path.join(schoolIdDir, '/enrollment.json')
+            await scrapeEndpointData(schoolId, year, `${baseUrl}/enrollment.php`, formData, enrollmentOutputFile)
+
+            const studentPopOutputFile = path.join(schoolIdDir, '/student_pop.json')
+            await scrapeEndpointData(schoolId, year, `${baseUrl}/student_pop.php`, formData, studentPopOutputFile)
+
+            const frameworkScoresOutputFile = path.join(schoolIdDir, '/framework_scores.json')
+            await scrapeEndpointData(schoolId, year, `${baseUrl}/framework_scores.php`, formData, frameworkScoresOutputFile)
+
+            const percentileRankOutputFile = path.join(schoolIdDir, '/percentile_rank.json')
+            await scrapeEndpointData(schoolId, year, `${baseUrl}/percentile_rank.php`, formData, percentileRankOutputFile)
+
+        } catch (error) {
+            console.error(`Error getting data - ${error}`)
+        }
+    }
+}
 
 export const scrapeAllData = async () => {
     const baseDir = path.join(__dirname, '../data')
 
     // Impact performance
-    // try {
-    //     const response = await axios.get(`${baseUrl}/impact_performance.php`)
-    //     const outputDir = path.join(baseDir, '/2018')
-    //     if (!fs.existsSync(outputDir)){
-    //         fs.mkdirSync(outputDir);
-    //     }
-    //     fs.writeFileSync(path.join(outputDir, '/impactPerformance.json'), JSON.stringify({data: response.data}))
-    //     return Promise.resolve(true)
-    // } catch (error) {
-    //     console.error(error)
-    //     return Promise.reject(error.message)
-    // }
     try {
-        const schoolsArray = JSON.parse(fs.readFileSync(`${baseDir}/schools.json`))
-        const numSchools = schoolsArray.length
-    
+        const outputDir = path.join(baseDir, '/2018')
+            if (!fs.existsSync(outputDir)){
+                fs.mkdirSync(outputDir);
+            }
+        const output = path.join(outputDir, '/impactPerformance.json')
+        if(fs.existsSync(output)){
+            console.log(`Impact performance data already exists`)
+        } else {
+            const response = await axios.get(`${baseUrl}/impact_performance.php`)
+            fs.writeFileSync(output, JSON.stringify({data: response.data}))
+        }
+    } catch (error) {
+        console.error(`Error scraping impact performance data - ${error}`)
+        return Promise.reject(error.message)
+    }
+    // Other data 
+    try {
+        const schoolsArray = JSON.parse(fs.readFileSync(`${baseDir}/schoolData.json`))    
         for(let year = startYear; year <= endYear; year++) {
             try {
-                console.log(`getting data for year: ${year}`)
                 const outputDir = path.join(baseDir, `/${year}`)
                 if (!fs.existsSync(outputDir)){
                     fs.mkdirSync(outputDir);
                 }
-                for(let schoolIndex = 0; schoolIndex < numSchools; schoolIndex++){
-                    const schoolObj = schoolsArray[schoolIndex]
-                    const schoolId = schoolObj.id
-                    console.log(`getting data for school ID: ${schoolId}`)
-                }
+                await scrapeDataForYear(schoolsArray, year, outputDir)
     
             } catch (error) {
                 console.error(`Error getting data for year ${year} - ${error}`)
             }
         }
+        console.log('done!')
         Promise.resolve(true)
     } catch(error) {
         console.error(error)
