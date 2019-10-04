6c4f98a5dad3894efda4553b39ce2c40073a4fb2
Yuriy Vaskevich <yvaskevich@amplify.com>
2019-10-03 20:11:26 -0400

    Added basic school list

--- frontend/src/components/Schools.jsx
+++ frontend/src/components/Schools.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import '../styles/Schools.scss'
 
 export default class Schools extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            schools: []
+        }
+    }
+
+    async componentDidMount() {
+        const schools = await axios.get('http://localhost:9000/data/schoolData.json')
+        this.setState({ schools: schools.data })
+    }
+
     render() {
         return (
             <ul>
-                <li>School A</li>
-                <li>School B</li>
-                <li>School C</li>
-                <li>School D</li>
-                <li>School E</li>
+                {this.state.schools.map(school => (<li key={school.dbn + '-' + school.report_type}>{school.school}</li>))}
             </ul>
         )
     }
